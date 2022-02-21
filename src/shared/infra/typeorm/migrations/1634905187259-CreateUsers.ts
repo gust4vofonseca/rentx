3f30723af293595fc06034948d61d7aeb3b446dd
Gustavo Fonseca <44001877+gust4vofonseca@users.noreply.github.com>
2022-02-20 21:31:06 -0300

    rental

--- src/shared/infra/typeorm/migrations/1634905187259-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1634905187259-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1634905187259 implements MigrationInterface {
                     {
                         name: "id",
                         type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
